59342b3f2e7562c0c5c86c53217f8a4d9530709e
LinNes <lnestorson@gmail.com>
2021-08-27 08:23:20 +0200

    cleaning up javascript syntax

--- javascript/logic.js
+++ javascript/logic.js
@@ -4,28 +4,27 @@ slides[1].style.display = "block";
 
 function nextSlide() {
     if (slidenumber + 1 < slides.length) {
-        slides[slidenumber].style.display = "none";
-        slides[slidenumber + 1].style.display = "block";
+        slideManagement(slidenumber + 1);
         slidenumber++;
     }
     else if (slidenumber + 1 >= slides.length) {
-        slides[slidenumber].style.display = "none";
-        slides[0].style.display = "block";
+        slideManagement(0);
         slidenumber = 0;
     }
 }
 
 function prevSlide() {
     if (slidenumber - 1 >= 0) {
-        slides[slidenumber].style.display = "none";
-        slides[slidenumber - 1].style.display = "block";
+        slideManagement(slidenumber - 1);
         slidenumber--;
     }
     else if (slidenumber - 1 < 0) {
-        slides[slidenumber].style.display = "none";
-        slides[slides.length - 1].style.display = "block";
+        slideManagement(slides.length - 1);
         slidenumber = 2;
     }
 }
 
-
+function slideManagement(x) {
+    slides[slidenumber].style.display = "none";
+    slides[x].style.display = "block";
+}
